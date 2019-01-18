8e3bddde2e0ed2d901e7f174b0679cb21a9033f9
adiatullin <adiatullin@haulmont.com>
2019-01-18 18:56:32 +0400

    Refactoring and renaming

--- modules/web/src/com/haulmont/addon/saml/saml/authentication/SamlAuthenticationSuccessHandler.java
+++ modules/web/src/com/haulmont/addon/saml/saml/authentication/SamlAuthenticationSuccessHandler.java
@@ -13,15 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.haulmont.addon.saml.saml.authentication;
 
 import com.haulmont.addon.saml.security.SamlSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.saml.SAMLCredential;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
@@ -29,25 +26,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static com.haulmont.addon.saml.web.security.saml.SamlSessionPrincipal.*;
+import static com.haulmont.addon.saml.web.security.saml.SamlSessionPrincipal.SAML_CONNECTION_CODE;
+import static com.haulmont.addon.saml.web.security.saml.SamlSessionPrincipal.SAML_ERROR_ATTRIBUTE;
+import static com.haulmont.addon.saml.web.security.saml.SamlSessionPrincipal.SAML_SESSION_ATTRIBUTE;
 
 /**
- * @author kuchmin
+ * @author adiatullin
  */
-@Component("saml_SamlSessionAttributeHandler")
-public class SamlSessionAttributeHandle implements AuthenticationSuccessHandler {
-
-    protected Logger log = LoggerFactory.getLogger(getClass());
+@Component("successRedirectHandler")
+public class SamlAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+        createSamlSession(request, response, authentication);
+        cleanupSessionAttributes(request, response, authentication);
+
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+
+    protected void createSamlSession(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
         SamlSession samlSession = new SamlSession();
         samlSession.setPrincipal((String) authentication.getPrincipal());
         samlSession.setCredential((SAMLCredential) authentication.getCredentials());
         samlSession.setConnectionCode((String) request.getSession().getAttribute(SAML_CONNECTION_CODE));
+        request.getSession().setAttribute(SAML_SESSION_ATTRIBUTE, samlSession);
+    }
+
+    protected void cleanupSessionAttributes(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
         request.getSession().removeAttribute(SAML_CONNECTION_CODE);
         request.getSession().removeAttribute(SAML_ERROR_ATTRIBUTE);
-
-        request.getSession().setAttribute(SAML_SESSION_ATTRIBUTE, samlSession);
     }
 }
