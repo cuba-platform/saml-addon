159171760701cea5fd340c9154d9a763d508347e
strokov <strokov@haulmont.com>
2019-03-07 15:38:49 +0400

    update scripts for postgres

--- modules/core/db/init/postgres/create-db.sql
+++ modules/core/db/init/postgres/create-db.sql
@@ -1,32 +1,31 @@
 -- begin SAMLADDON_SAML_CONNECTION
 create table SAMLADDON_SAML_CONNECTION (
-    ID uuid not null,
+    ID uuid,
+    VERSION integer not null,
     CREATE_TS timestamp,
     CREATED_BY varchar(50),
-    VERSION integer,
     UPDATE_TS timestamp,
     UPDATED_BY varchar(50),
     DELETE_TS timestamp,
     DELETED_BY varchar(50),
     --
     NAME varchar(255) not null,
+    CREATE_USERS boolean,
     SSO_PATH varchar(100) not null,
     SP_ID varchar(255) not null,
-    ACTIVE boolean default false,
-    CREATE_USERS boolean default true,
-    KEYSTORE_ID uuid,
-    KEYSTORE_PASSWORD varchar(255),
+    ACTIVE boolean,
     IDP_METADATA_URL varchar(255),
     IDP_METADATA_ID uuid,
+    DEFAULT_GROUP_ID uuid not null,
     PROCESSING_SERVICE varchar(255) not null,
+    KEYSTORE_ID uuid,
     --
     primary key (ID)
 )^
 -- end SAMLADDON_SAML_CONNECTION
-
 -- begin SAMLADDON_KEY_STORE
 create table SAMLADDON_KEY_STORE (
-    ID uuid not null,
+    ID uuid,
     VERSION integer not null,
     CREATE_TS timestamp,
     CREATED_BY varchar(50),
@@ -44,6 +43,7 @@ create table SAMLADDON_KEY_STORE (
 )^
 -- end SAMLADDON_KEY_STORE
 
+
 -- begin SAMLADDON_SAML_CONNECTION
 alter table SAMLADDON_SAML_CONNECTION add constraint FK_SAMLADDON_SAML_CONNECTION_ON_IDP_METADATA foreign key (IDP_METADATA_ID) references SYS_FILE(ID)^
 alter table SAMLADDON_SAML_CONNECTION add constraint FK_SAMLADDON_SAML_CONNECTION_ON_DEFAULT_GROUP foreign key (DEFAULT_GROUP_ID) references SEC_GROUP(ID)^
@@ -52,9 +52,7 @@ create index IDX_SAMLADDON_SAML_CONNECTION_ON_IDP_METADATA on SAMLADDON_SAML_CON
 create index IDX_SAMLADDON_SAML_CONNECTION_ON_DEFAULT_GROUP on SAMLADDON_SAML_CONNECTION (DEFAULT_GROUP_ID)^
 create index IDX_SAMLADDON_SAML_CONNECTION_ON_KEYSTORE on SAMLADDON_SAML_CONNECTION (KEYSTORE_ID)^
 -- end SAMLADDON_SAML_CONNECTION
-
 -- begin SAMLADDON_KEY_STORE
 alter table SAMLADDON_KEY_STORE add constraint FK_SAMLADDON_KEY_STORE_ON_KEY foreign key (KEY_ID) references SYS_FILE(ID)^
 create index IDX_SAMLADDON_KEY_STORE_ON_KEY on SAMLADDON_KEY_STORE (KEY_ID)^
 -- end SAMLADDON_KEY_STORE
-----------------------------------------------------------------------------------------------------------------
