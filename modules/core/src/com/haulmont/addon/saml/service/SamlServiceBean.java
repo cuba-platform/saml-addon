f1204bc96580821d24ed79bf9e979f6daee29fcd
MikhailStrokov <strokov@haulmont.com>
2019-08-12 16:21:57 +0400

    Upgraded add-on to CUBA 7.1 platform version #9

--- modules/core/src/com/haulmont/addon/saml/service/SamlServiceBean.java
+++ modules/core/src/com/haulmont/addon/saml/service/SamlServiceBean.java
@@ -32,10 +32,10 @@ import org.opensaml.DefaultBootstrap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import javax.transaction.Transactional;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
