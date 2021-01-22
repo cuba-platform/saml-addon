dc965957f555277458d0438ad99137907dfbcd93
shalyganov <shalyganov@haulmont.com>
2021-01-22 16:52:17 +0400

    Update scripts for MSSQL and Oracle are invalid #27

--- modules/core/db/update/mssql/20/201022-2-updateKeyStore.sql
+++ modules/core/db/update/mssql/20/201022-2-updateKeyStore.sql
@@ -1,1 +1,1 @@
-alter table SAMLADDON_KEY_STORE add column KEYSTORE_PASSWORD varchar(100) ^
+alter table SAMLADDON_KEY_STORE add KEYSTORE_PASSWORD varchar(100) ^
--- modules/core/db/update/oracle/20/201022-2-updateKeyStore.sql
+++ modules/core/db/update/oracle/20/201022-2-updateKeyStore.sql
@@ -1,1 +1,1 @@
-alter table SAMLADDON_KEY_STORE add column KEYSTORE_PASSWORD varchar2(100) ;
+alter table SAMLADDON_KEY_STORE add KEYSTORE_PASSWORD varchar2(100) ;
